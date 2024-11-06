6817cadafaa573b6e13385aff45aa2bbaf0727c2
Sofía Canizales <clsofi64@gmail.com>
2024-11-06 18:18:40 -0500

    merges

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-import ConsumoAPi from './components/functionals/ConsumoApi'
-=======
 import Router from "../src/routes/Router";
-
 
 import "../src/styles/global.css";
 
->>>>>>> dev
 function App() {
   return (
     <> 
